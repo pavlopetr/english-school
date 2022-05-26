2462181fc905964e50cc1665aede09b60e2eba4d
SergejShk <sshkurupij11@gmail.com>
2022-05-26 13:07:56 +0300

    change image folder

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,19 +1,19 @@
-$('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav',
-    prevArrow: $('slick-prev-js'),
-    nextArrow: $('slick-next-js'),
-  });
-  $('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true,
-    arrows: false,
-    centerPadding: '0px'
-  });
+$('.slider-for-js').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav-js',
+  prevArrow: $('slick-prev-js'),
+  nextArrow: $('slick-next-js'),
+});
+$('.slider-nav-js').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for-js',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  arrows: false,
+  centerPadding: '0px',
+});
